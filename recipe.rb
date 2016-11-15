34ba5226241b9942c108504a3b8173c118f523f2
Mick Pollard <aussielunix@gmail.com>
2016-11-15 11:15:00 +1100

    fixup hugo url

--- recipe.rb
+++ recipe.rb
@@ -11,7 +11,7 @@ class Hugo < FPM::Cookery::Recipe
   description 'A Fast, modern, static website engine.'
   section     'admin'
 
-  source      "https://github.com/spf13/hugo/releases/download/v#{version}/hugo-linux-amd64"
+  source      'https://github.com/spf13/hugo/releases/download/v#{version}/hugo_#{version}_Linux-64bit.tar.gz'
   sha256      'a4ff87f3b16c786226e0d1b3a2862246080ca1a608a2e2b676166cf0ef929001'
 
   def build
