f5f5cb2b1247f34a1635f50b66b2c2d9960b6718
Mick Pollard <aussielunix@gmail.com>
2016-11-15 11:05:43 +1100

    updated version

--- recipe.rb
+++ recipe.rb
@@ -11,15 +11,15 @@ class Hugo < FPM::Cookery::Recipe
   description 'A Fast, modern, static website engine.'
   section     'admin'
 
-  source      "https://github.com/spf13/hugo/releases/download/v#{version}/hugo_#{version}_linux_amd64.tar.gz"
-  sha256      '27f4de82e490dfbe0f81eee280f94878e2fd71f2716d7d067fd9c57816ac309a'
+  source      "https://github.com/spf13/hugo/releases/download/v#{version}/hugo-linux-amd64"
+  sha256      'a4ff87f3b16c786226e0d1b3a2862246080ca1a608a2e2b676166cf0ef929001'
 
   def build
   end
 
   def install
     mkdir_p opt
-    cp_r builddir("hugo_#{version}_linux_amd64"), opt('hugo')
+    cp_r builddir("hugo-linux-amd64"), opt('hugo')
     etc('profile.d').install workdir('hugo.profile'), 'hugo.sh'
   end
 end
